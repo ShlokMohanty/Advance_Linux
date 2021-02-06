806fc26980629faeb5001102a41c050b5d05fe9d
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-02-07 00:51:49 +0530

    added some more commands for the directories.

--- directories.sh
+++ directories.sh
@@ -1,1 +1,5 @@
 shlok@DESKTOP-I6ELKPB $ pwd          output-->/home/shlok
+shlok@DESKTOP-I6ELKPB $ ls           output--->file1.tar file2.txt
+shlok@DESKTOP-I6ELKPB $ mkdir shlok_linux_files       output--->shlok@DESKTOP-I6ELKPB $ shlok_linux_files$
+shlok@DESKTOP-I6ELKPB $rmdir shlok_linux_files      output-->Folder gets removed .
+shlok@DESKTOP-I6ELKPB $
